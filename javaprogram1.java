906c303b5b9ab21769530620205c26db969866a2
shalini1789 <72307356+shalini1789@users.noreply.github.com>
2020-10-13 09:18:49 +0530

    Update javaprogram1.java

--- javaprogram1.java
+++ javaprogram1.java
@@ -1,8 +1,9 @@
- public class test{
+ public class javaprogram1{
    
    public static void main(String[] args)
    {
    
+    System.out.println("welcome to core java!!!!");
     System.out.println("welcome to core java!!!!");
    }
  }
